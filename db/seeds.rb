50f75d467a27b53efa4bb1ba12b6458993c15fa6
YuhriParada <yuhriparada@gmail.com>
2021-08-18 19:15:29 -0300

    app is working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'cleaning up database...'
+Restaurant.destroy_all
+puts 'database is clean!'
+
+puts "Creating 5 restaurants...."
+
+5.times do
+	restaurant = Restaurant.create(
+		name: Faker::TvShows::SiliconValley.company,
+		address: Faker::Address.city,
+		category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+		rating: rand(1..5,)
+		phone_number: Faker::PhoneNumber.cell_phone
+	)
+	puts "Restaurant #{restaurant.id} is created"
+end
+
+puts "Finish !"
